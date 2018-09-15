d38c16b7316ce0cf792688847d279717c87c3f17
Yohei-T <yohei.tsukuda@outlook.jp>
2018-09-16 01:58:55 +0900

    StepAsideUnityChan 1回目のコミット
    発展課題まで達成。

---
+++ Assets/ConeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConeController : MonoBehaviour {
+
+    private GameObject unitychan;
+    //Unityちゃんと物体の距離
+    private float difference;
+
+    // Use this for initialization
+    void Start () {
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Unityちゃんと物体の位置（z座標）の差を求める
+        this.difference = unitychan.transform.position.z - this.transform.position.z;
+        if (difference > 10)
+        {
+            Destroy(this);
+        }
+    }
+}
